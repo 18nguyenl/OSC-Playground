6c0b6a9e14a23b279e93fcf3136c92faa86b3d4d
Atelier Member <hybrid.atelier@uta.edu>
2020-11-20 03:01:27 -0600

    Try to get multi dimension OSC to work

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-(function(LaunchControl) {
-  "use strict";
+import { Server, Client, Message } from 'node-osc';
 
-  var ctrl = new LaunchControl();
+const client = new Client('192.168.1.24', 8000);
+var oscServer = new Server(3333, '0.0.0.0', () => {
+  console.log('OSC Server is listening');
+});
 
-  Promise.resolve().then(function() {
-    return ctrl.open();
-  }).then(function() {
-    ctrl.on("message", function(e) {
-      console.log(JSON.stringify(e));
-    });
-  }).catch(function(e) {
-    console.log("ERROR: " + e.toString());
-  });
+oscServer.on('message', function (msg) {
+  console.log(msg);
+  switch (msg[0]) {
+    case '/1/fader1':
+      
+      break;
+    case '/1/fader4':
+      client.send('/Slide/loopCount', msg[1] * 20, (err) => {
+        if (err) {
+          console.log(err);
+        }
+      })
+      break;
+    case '/3/xy':
+      client.send('/Slide/base', msg[0], msg[1], 1, 1)
+    case '/1/toggle4':
+      if (msg[1] === 1)
+        client.send('/Slide/base', 0, 1, 0, 1) 
+      else if (msg[1] === 0)
+        client.send('/Slide/base', 1, 1, 0, 1);
+  }
 
-})((this.self || global).LaunchControl || require("@mohayonao/launch-control").default);
+  // oscServer.close();
+});
